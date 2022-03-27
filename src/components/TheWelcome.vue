27bfe44f40f98e8b311f1da79bbcf4cb099945ea
hatpoppo <toshi.hat@gmail.com>
2022-03-28 08:02:32 +0900

    Computed Property Basic sample.

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,6 +1,7 @@
 <script setup>
 import WelcomeItem from './WelcomeItem.vue'
 import Count from './Count.vue'
+import ComputedProperty from './ComputedProperty.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
 import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
@@ -10,6 +11,7 @@ import SupportIcon from './icons/IconSupport.vue'
 
 <template>
   <Count></Count>
+  <ComputedProperty></ComputedProperty>
   <WelcomeItem>
     <template #icon>
       <DocumentationIcon />
