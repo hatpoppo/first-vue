e24a595932ee2c17d1d3a8fdf0c92fd99a0e99bc
hatpoppo <toshi.hat@gmail.com>
2022-03-27 17:46:59 +0900

    Declaring Reactive State sample.

---
+++ src/components/Count.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { reactive } from 'vue';
+const state = reactive({ count: 0 })
+function increment() {
+  state.count++
+}
+</script>
+<template>
+  <button @click="increment">{{ state.count }}</button>
+</template>
