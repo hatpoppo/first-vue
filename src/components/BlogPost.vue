cf5401efc35c649c99d231a5f6a9891eb7a9c7a4
hatpoppo <toshi.hat@gmail.com>
2022-04-04 07:37:06 +0900

    Components Basics.

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from "vue";
+defineProps(['title'])
+</script>
+<template>
+  <div class="parts">
+    <h2>BlogPost</h2>
+    <p>{{ title }}</p>
+  </div>
+</template>
---
+++ src/components/ComponentsBasics.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+import BlogPost from './BlogPost.vue'
+
+const posts = ref([
+  { id: 1, title: 'My Page' },
+  { id: 2, title: 'Your Page' },
+  { id: 3, title: 'All Pages' }
+])
+</script>
+<template>
+  <div class="parts">
+    <h2>ComponentsBasics</h2>
+    <BlogPost v-for="post in posts" :key="post.id" :title="post.title" />
+  </div>
+</template>
